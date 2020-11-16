1c83db757f33f6a7f9cc57f3105c7f6c9c6381d9
Gerald Chau <mwoit@ymail.com>
2020-11-16 22:46:23 +0000

    Let's learn some C#.

---
+++ tutorial/c#/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n君のン前は何ですか? ");
+            string name = Console.ReadLine();
+            var date = DateTime.Now;
+            var greeting = date.Hour < 12 ? "こんにち" : "今晩";
+            Console.WriteLine($"\n{greeting}, {name}, {date:d}日、 {date:t}時!");
+            Console.Write($"何れかのキーを押してください！");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ tutorial/c#/HelloWorld/README.md
@@ -0,0 +1,8 @@
+Source:
+
+* https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code
+
+Steps
+
+1. Execute `dotnet new console` in terminal.
+2. `dotnet run`.
